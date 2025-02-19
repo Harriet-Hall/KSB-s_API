0246ea1b2d80cf5c55e2d364e67695d44048d126
Harriet Hall <harriet.hall@madetech.com>
2025-02-19 13:54:05 +0000

    test: green - test ksb_type_validator

--- app/unit_tests/test_model.py
+++ app/unit_tests/test_model.py
@@ -4,3 +4,22 @@ def test_Ksb_model():
     Ksb_instance = Ksb()
     assert isinstance(Ksb_instance, Model)
     assert isinstance(Ksb_instance, BaseModel)
+    
+def test_ksb_properties_have_correct_types():
+  ksb = Ksb(id = "acde070d-8c4c-4f0d-9d8a-162843c10333", ksb_type = "knowledge", ksb_code = 1, description = "Test description")
+  assert isinstance(ksb.id, str)
+  assert isinstance(ksb.ksb_type, str)
+  assert isinstance(ksb.ksb_code, int)
+  assert isinstance(ksb.description, str)
+  
+  
+def test_ksb_type_validator():
+  ksb_type_values = ['Knowledge', 'knowledge', 'Skill', 'skill', 'Behaviour', 'behaviour']
+
+  for i, ksb_type_value in enumerate(ksb_type_values):
+    ksb = Ksb(id = "acde070d-8c4c-4f0d-9d8a-162843c10333", ksb_type = ksb_type_value, ksb_code = 1, description = "Test description")
+    ksb.ksb_type_validator()
+    assert ksb.ksb_type == ksb_type_values[i].capitalize()
+  
+  
+  
