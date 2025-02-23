9d0137ed216dbbfe8e5823e01e0bc2485f210e0d
Harriet Hall <harriet.hall@madetech.com>
2025-02-23 16:45:18 +0000

    add .capitalize() to post_payload

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
 def check_for_duplicates(ksbs, post_payload):
             for ksb in ksbs:
-                if ksb.ksb_type == post_payload.json["ksb_type"] and ksb.ksb_code == post_payload.json["ksb_code"]:
+                if ksb.ksb_type == post_payload.json["ksb_type"].capitalize() and ksb.ksb_code == post_payload.json["ksb_code"]:
                     return True
