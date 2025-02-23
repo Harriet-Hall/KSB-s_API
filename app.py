33c1ccf85e72622b95358a425981d89ea65c2b91
Harriet Hall <harriet.hall@madetech.com>
2025-02-23 16:05:52 +0000

    refactor app.py: add check_for_duplicates utils function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from database import Ksb
+from utils import check_for_duplicates
 
 app = Flask(__name__)
 
@@ -23,29 +24,35 @@ def ksbs():
         return jsonify(ksbs)
 
     elif request.method == "POST":
-        new_row = Ksb.create(**request.json)
-        new_ksb = Ksb.select().where(
-            Ksb.ksb_type == new_row.ksb_type,
-            Ksb.ksb_code == new_row.ksb_code,
-            Ksb.description == new_row.description,
-        )
-        if len(new_ksb) > 1:
+        ksbs = Ksb.select()
+        
+        if check_for_duplicates(ksbs, request):
             response = jsonify({"error" : "Ksb already exists in database"})
             response.status_code = 409
             return response
-        
-        ksb = new_ksb[0]
-        response = jsonify(
-            {
-                "id": ksb.id,
-                "type": ksb.ksb_type,
-                "code": ksb.ksb_code,
-                "description": ksb.description,
-            }
-        )
+    
+        else:
+            new_row = Ksb.create(**request.json).save()
 
-        response.status_code = 201
-        return response
+            new_ksb = Ksb.select().where(
+                Ksb.ksb_type == new_row.ksb_type,
+                Ksb.ksb_code == new_row.ksb_code,
+                Ksb.description == new_row.description,
+            )
+
+            
+            ksb = new_ksb[0]
+            response = jsonify(
+                {
+                    "id": ksb.id,
+                    "type": ksb.ksb_type,
+                    "code": ksb.ksb_code,
+                    "description": ksb.description,
+                }
+            )
+
+            response.status_code = 201
+            return response
 
 
 @app.route("/<ksb_type>")
