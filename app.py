7936089562055d5b9ad65dd9aaabc8ca20878f17
Harriet Hall <harriet.hall@madetech.com>
2025-02-24 12:37:06 +0000

    refactor: return logic made dry

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ from utils import check_for_duplicates, KSB_TYPE_CHOICES
 
 app = Flask(__name__)
 
-# @app.errorhandler(404)
-# def not_found(error):
-#     return jsonify({'error': 'Not found'}), 404
 
 @app.route("/", methods=["GET", "POST", "DELETE"])
 def ksbs():
@@ -33,9 +30,7 @@ def ksbs():
         ksbs = Ksb.select()
         
         if check_for_duplicates(ksbs, request):
-            response = jsonify({"error" : "Ksb already exists in database"})
-            response.status_code = 409
-            return response
+            return jsonify({"error" : "Ksb already exists in database"}), 409
 
         else:
             try:
@@ -62,9 +57,7 @@ def ksbs():
                 response.status_code = 201
                 return response
             except ValueError as value_error:
-                response = jsonify({"error": str(value_error)})
-                response.status_code = 400
-                return response
+                return jsonify({"error": str(value_error)}), 400
 
     elif request.method == "DELETE":
         
