b3305e24e820b6e263bb89fd4830f88d61e82327
Harriet Hall <harriet.hall@madetech.com>
2025-02-25 18:16:32 +0000

    refactor: remove unecessary logic

--- utils/check_for_duplicates.py
+++ utils/check_for_duplicates.py
@@ -1,22 +1,10 @@
-from database import Ksb
-
 def check_for_duplicates(ksbs, post_payload):
-    if isinstance(post_payload, dict):
-        type = post_payload["ksb_type"].capitalize()
-        code = post_payload["ksb_code"]
-        description = post_payload["description"]
-    elif isinstance(post_payload, Ksb):
-        type = post_payload.ksb_type
-        code = post_payload.ksb_code
-        description = post_payload.description
-    else:
-        raise ValueError("Invalid post_payload type")
+
+    type = post_payload["ksb_type"].capitalize()
+    code = post_payload["ksb_code"]
+    description = post_payload["description"]
 
     for ksb in ksbs:
-        if (
-            ksb.ksb_type == type and
-            ksb.ksb_code == code and
-            ksb.description == description
-        ):
-            return True 
-    return False  
+        if ksb.ksb_type == type and ksb.ksb_code == code and ksb.description == description:
+            return True
+            
