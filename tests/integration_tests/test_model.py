7ec9fc3bccdd9de5c1e0a6c4bc7030e360eccf53
Harriet Hall <harriet.hall@madetech.com>
2025-02-25 08:54:23 +0000

    format : prettier

--- tests/integration_tests/test_model.py
+++ tests/integration_tests/test_model.py
@@ -8,54 +8,55 @@ psql_test_db = PostgresqlDatabase(
     host=os.getenv("TEST_HOST"),
     user=os.getenv("TEST_USER"),
     password=os.getenv("TEST_PASSWORD"),
-    port=os.getenv("TEST_PORT")
+    port=os.getenv("TEST_PORT"),
 )
 
-     
+
 @pytest.fixture(scope="function")
 def test_database():
     psql_test_db.bind([Ksb])
     psql_test_db.connect()
     with psql_test_db.transaction() as transaction:
-        try:     
-            yield transaction 
-        finally: 
+        try:
+            yield transaction
+        finally:
             transaction.rollback()
     psql_test_db.close()
 
 
-
 def test_table_is_seeded(test_database):
-        rows = Ksb.select()
-        assert len(rows) == 4
-        
+    rows = Ksb.select()
+    assert len(rows) == 4
+
 
 def test_create_ksb_entry(test_database):
 
-    Ksb.create(
-        ksb_type= "Knowledge",
-        ksb_code= 1,
-        description="Test description"
-    )
-    
+    Ksb.create(ksb_type="Knowledge", ksb_code=1, description="Test description")
+
     rows = Ksb.select()
     assert len(rows) == 5
 
     new_row = rows[4]
-    assert new_row.id 
+    assert new_row.id
     assert new_row.ksb_type == "Knowledge" and isinstance(new_row.ksb_type, str)
     assert new_row.ksb_code == 1 and isinstance(new_row.ksb_code, int)
-    assert new_row.description == "Test description" and isinstance(new_row.description, str)
-    
+    assert new_row.description == "Test description" and isinstance(
+        new_row.description, str
+    )
 
 
 def test_create_ksb_entry_with_valid_ksb_types(test_database):
-    ksb_type_values = ['Knowledge', 'knowledge', 'Skill', 'skill', 'Behaviour', 'behaviour']
+    ksb_type_values = [
+        "Knowledge",
+        "knowledge",
+        "Skill",
+        "skill",
+        "Behaviour",
+        "behaviour",
+    ]
     for ksb_type_value in ksb_type_values:
         Ksb.create(
-        ksb_type=f"{ksb_type_value}",
-        ksb_code= 1,
-        description="Test description"
+            ksb_type=f"{ksb_type_value}", ksb_code=1, description="Test description"
         )
     rows = Ksb.select()
 
@@ -64,45 +65,51 @@ def test_create_ksb_entry_with_valid_ksb_types(test_database):
     for i, row in enumerate(new_rows):
         assert row.ksb_type == ksb_type_values[i].capitalize()
 
-    
+
 def test_error_raised_when_ksb_type_is_invalid(test_database):
-    
+
     with pytest.raises(ValueError) as value_error:
         invalid_types = ["", 123, "ski1111"]
         for type in invalid_types:
-            
-            Ksb.create(
-            ksb_type=type,
-            ksb_code= 1,
-            description="Test description"
-             )
-            
+
+            Ksb.create(ksb_type=type, ksb_code=1, description="Test description")
+
     assert str(value_error.value) == f"{type} is not a valid ksb_type"
-        
+
 
 def test_create_ksb_entry_with_valid_ksb_code(test_database):
-        Ksb.create(
+    Ksb.create(
         ksb_type="knowledge",
-        ksb_code= 1,
+        ksb_code=1,
         description="Test description",
-        )
-        row = Ksb.select()
-        assert row[4].ksb_code == 1
-        
+    )
+    row = Ksb.select()
+    assert row[4].ksb_code == 1
+
+
 def test_create_ksb_entry_with_invalid_ksb_code_string(test_database):
     with pytest.raises(ValueError) as value_error:
         Ksb.create(ksb_type="knowledge", ksb_code="3", description="Test description")
     assert str(value_error.value) == "3 is of type: str, it needs to be an integer"
 
+
 def test_create_ksb_entry_with_invalid_ksb_code_out_of_range_low(test_database):
     with pytest.raises(ValueError) as value_error:
         Ksb.create(ksb_type="knowledge", ksb_code=0, description="Test description")
-    assert str(value_error.value) == "0 is not a valid ksb_code, choose an int from 1 to 50"
+    assert (
+        str(value_error.value)
+        == "0 is not a valid ksb_code, choose an int from 1 to 50"
+    )
+
 
 def test_create_ksb_entry_with_invalid_ksb_code_out_of_range_high(test_database):
     with pytest.raises(ValueError) as value_error:
         Ksb.create(ksb_type="knowledge", ksb_code=51, description="Test description")
-    assert str(value_error.value) == "51 is not a valid ksb_code, choose an int from 1 to 50"
+    assert (
+        str(value_error.value)
+        == "51 is not a valid ksb_code, choose an int from 1 to 50"
+    )
+
 
 def test_create_ksb_entry_with_invalid_ksb_code_empty_string(test_database):
     with pytest.raises(ValueError) as value_error:
@@ -111,24 +118,23 @@ def test_create_ksb_entry_with_invalid_ksb_code_empty_string(test_database):
 
 
 def test_create_ksb_entry_with_valid_ksb_description(test_database):
-        Ksb.create(
+    Ksb.create(
         ksb_type="knowledge",
-        ksb_code= 1,
+        ksb_code=1,
         description="Test description",
-        )
-        row = Ksb.select()
-        assert row[4].description == "Test description"
-    
+    )
+    row = Ksb.select()
+    assert row[4].description == "Test description"
+
+
 def test_create_ksb_entry_with_invalid_ksb_description(test_database):
-    
-    
+
     with pytest.raises(ValueError) as value_error:
-        invalid_descriptions= ["Too short", ("i" * 301), ""]
+        invalid_descriptions = ["Too short", ("i" * 301), ""]
         for description in invalid_descriptions:
-                
-            Ksb.create(
-            ksb_type="knowledge",
-            ksb_code=12,
-            description=description
-            )
-    assert str(value_error.value) == "description needs to be more than 15 characters and less than 300 characters in length"
+
+            Ksb.create(ksb_type="knowledge", ksb_code=12, description=description)
+    assert (
+        str(value_error.value)
+        == "description needs to be more than 15 characters and less than 300 characters in length"
+    )
