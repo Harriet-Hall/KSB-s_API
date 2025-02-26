0a16848db7df69f58b0d57b3f0b462aaba5866e1
Harriet Hall <harriet.hall@madetech.com>
2025-02-26 13:59:29 +0000

    update imports

--- app/database.py
+++ app/database.py
@@ -1,10 +1,11 @@
 from flask import app
 from peewee import *
 import os 
-from .utils.ksb_type_choices import KSB_TYPE_CHOICES
+from utils.ksb_type_choices import KSB_TYPE_CHOICES
 from dotenv import load_dotenv
 
 load_dotenv()
+
 
 psql_db = PostgresqlDatabase(
     os.getenv("DATABASE"),
